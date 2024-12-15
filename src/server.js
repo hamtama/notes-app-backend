5d7d3ed3bd54d5ddf42433c6a43526fd7c03a55d
hamtama <mungkinbikinlagi@gmail.com>
2024-12-15 18:28:41 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -12,10 +12,12 @@
     const init = async () => {
         const server = Hapi.server({
             port: 5000,
-            host: 'localhost',
+            // host: 'localhost',
+            host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
             routes: {
                 cors: {
-                    origin: ['http://notesapp-v1.dicodingacademy.com'],
+                    origin: ['*'],
+                    // origin: ['http://notesapp-v1.dicodingacademy.com'],
                     // credentials: true
                 },
             },
